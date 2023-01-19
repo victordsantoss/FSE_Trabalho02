ced49d2d8356ff1a4269c03dd830c1e0ec5ad83e
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-01-18 23:21:49 -0300

    feat: adding uart connection

--- src/server.py
+++ src/server.py
@@ -1,5 +1,7 @@
-import RPI.GPIO as GPIO
+import RPi.GPIO as GPIO
 import serial
 
-
+# CONEXÃO COM A UART
+uart_connection = serial.Serial("/dev/serial0", baudrate=115200)
+print("uart_connection", uart_connection)
 
