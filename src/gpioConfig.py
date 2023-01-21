8d8906b7b53272971cf5b500a06e3f9e279ead0a
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-01-20 21:04:52 -0300

    chore: refactor gpio config

--- src/gpioConfig.py
+++ src/gpioConfig.py
@@ -1,13 +1,10 @@
 import RPi.GPIO as GPIO
 import json
 
-with open("../utils/config.json", encoding='utf-8') as meu_json: devices = json.load(meu_json)
-
-print (devices["outputs"][0]["gpio"])
-print (devices["outputs"][1]["gpio"])
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-GPIO.setup(devices["outputs"][0]["gpio"], GPIO.OUT)
-GPIO.setup(devices["outputs"][1]["gpio"], GPIO.OUT)
+def handleGPIOConfig():
+    with open("./utils/config.json", encoding='utf-8') as meu_json: devices = json.load(meu_json)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(devices["outputs"][0]["gpio"], GPIO.OUT)
+    GPIO.setup(devices["outputs"][1]["gpio"], GPIO.OUT)
+    return devices
